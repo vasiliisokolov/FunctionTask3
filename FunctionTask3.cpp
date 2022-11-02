00df8e5a512b8ed5d8cee9686b7174fb6aab775f
Vasily Sokolov <vasilysokolov@mail.ru>
2022-11-02 18:03:42 +0300

    Добавьте файлы проекта.

---
+++ FunctionTask3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int metering(int n, int k);
+
+int main()
+{
+    int n, k;
+    std::cout << "Enter ladder height and jump length: " << std::endl;
+    std::cin >> n >> k;
+    std::cout << metering(n, k);
+}
+
+int metering(int n, int k)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else if (n < 0 || k < 1)
+    {
+        return 0;
+    }
+    else
+    {
+        return metering(n - k, k) + metering(n, k - 1);
+    }
+    
+}
